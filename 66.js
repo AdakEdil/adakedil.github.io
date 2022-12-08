67c9dd6088c923c88fd7aed369d18389d912a143
AdakEdil <120076645+AdakEdil@users.noreply.github.com>
2022-12-08 10:17:49 +0600

    Add files via upload

---
+++ 66.js
@@ -0,0 +1,22 @@
+
+        	alert("қош келдіңіз")
+        	var test= prompt("Атыңызды енгізіңіз", "");
+        	switch(test){
+        		case("Answer"):
+        		alert("Жоқ")
+        		break;
+        		case("Password"):
+        		alert("Ия")
+        		break;
+        		default:
+        	}
+        	let Welcome= confirm("Қош келдіңіз!!!")
+        	alert(Welcome)
+
+
+
+            
+
+
+
+    
